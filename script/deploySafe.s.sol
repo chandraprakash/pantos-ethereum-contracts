751bbadaf7c38f8582688e72adbd0ca7866abed2
prakash <chandraprakashblr@gmail.com>
2024-07-17 09:54:05 +0100

    working copy

--- script/deploySafe.s.sol
+++ script/deploySafe.s.sol
@@ -8,30 +8,35 @@ import {Safe} from "@safe/Safe.sol";
 import {SafeProxyFactory} from "@safe/proxies/SafeProxyFactory.sol";
 import {SafeProxy} from "@safe/proxies/SafeProxy.sol";
 import {MultiSend} from "@safe/libraries/MultiSend.sol";
+import {Enum} from "@safe/libraries/Enum.sol";
+import {TestNFT} from "./helpers/TestNFT.sol";
 
 contract DeploySafe is Script {
-    address GNOSIS_SAFE_MASTER_COPY; // = 0x41675C099F32341bf84BFc5382aF534df5C7461a; // Replace with actual address
-    address PROXY_FACTORY; // = 0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67; // Replace with actual address
-    address MULTISEND; // = 0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526; // Replace with actual address
+    address GNOSIS_SAFE_MASTER_COPY;
+    address PROXY_FACTORY;
+    address MULTISEND;
 
-    address[] public owners;
-    uint256 public threshold;
+    
+    uint256 public threshold = 2;
+    mapping(address => bytes) public signatures;
 
-    function setUp() public {
-        // Set up the owners and threshold
-        owners = [0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266];
-        threshold = 1; // Number of required signatures
-    }
+    address[] public owners = [  // ANVIL's default accounts in ascending order
+        0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, // 0x3c44...
+        0x70997970C51812dc3A010C7d01b50e0d17dc79C8, // 0x70...
+        0x90F79bf6EB2c4f870365E785982E1f101E93b906  // 0x90...
+    ];
+    uint256[] private privateKeys = [ // ANVIL's default accounts private keys
+        0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a,
+        0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d,
+        0x7c8521182947a0b1ffdcf5e5babd128afdf80fbc5cdacbb0baed1bc56e75a6da
+    ];
 
     function run() external {
         vm.startBroadcast();
 
         // Deploy the Gnosis Safe master copy contract
         Safe gnosisSafe = new Safe();
-        console2.log(
-            "Gnosis Safe Master Copy deployed at:",
-            address(gnosisSafe)
-        );
+        console2.log("Gnosis Safe Master Copy deployed at:", address(gnosisSafe));
         GNOSIS_SAFE_MASTER_COPY = address(gnosisSafe);
 
         // Deploy the Proxy Factory contract
@@ -44,14 +49,8 @@ contract DeploySafe is Script {
         console2.log("MultiSend deployed at:", address(multiSend));
         MULTISEND = address(multiSend);
 
-        // // ProxyFactory proxyFactory = ProxyFactory(PROXY_FACTORY);
-        // bytes memory data = abi.encodeWithSignature("setup(address[],uint256,address,bytes,address,address,uint256,address)", owners, threshold, address(0), "", address(0), address(0), 0, address(0));
-
-        // // Deploy the proxy
-        // address safeProxy = proxyFactory.createProxy(GNOSIS_SAFE_MASTER_COPY, data);
-
         // Prepare data for Gnosis Safe setup
-        bytes memory data = abi.encodeWithSignature(
+        bytes memory setupData = abi.encodeWithSignature(
             "setup(address[],uint256,address,bytes,address,address,uint256,address)",
             owners,
             threshold,
@@ -67,12 +66,64 @@ contract DeploySafe is Script {
         uint256 saltNonce = 1;
         SafeProxy safeProxy = proxyFactory.createChainSpecificProxyWithNonce(
             GNOSIS_SAFE_MASTER_COPY,
-            data,
+            setupData,
             saltNonce
         );
 
-        vm.stopBroadcast();
+        address payable safeAddress = payable(address(safeProxy));
+        Safe safe = Safe(safeAddress);  // wrap proxy
 
-        console2.log("Gnosis Safe deployed at:", address(safeProxy));
+        require(safe.getOwners().length == owners.length, "Owners not set up correctly");
+        require(safe.getThreshold() == threshold, "Threshold not set correctly");
+
+        // Deploy TestNFT and make safe wallet its owner
+        TestNFT testNft = new TestNFT(safeAddress);
+
+        // Get transaction hash for pausing TestNFT
+        uint256 value = 0;
+        bytes memory data = abi.encodeWithSignature("pause()");
+
+        bytes32 txHash = safe.getTransactionHash(
+            address(testNft),
+            value,
+            data,
+            Enum.Operation.Call,
+            0,
+            0,
+            0,
+            address(0),
+            payable(address(0)),
+            safe.nonce()
+        );
+
+        console2.logBytes32(txHash);
+
+        // Sign the transaction 
+        bytes memory collectedSignatures;
+        for (uint256 i = 0; i < threshold; i++) {
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKeys[i], txHash);
+            bytes memory signature = abi.encodePacked(r, s, v);
+            collectedSignatures = abi.encodePacked(collectedSignatures, signature);
+        }
+
+        console2.logBytes(collectedSignatures);
+
+        // Execute the transaction
+        bool success = safe.execTransaction(
+            address(testNft),
+            value,
+            data,
+            Enum.Operation.Call,
+            0,
+            0,
+            0,
+            address(0),
+            payable(address(0)),
+            collectedSignatures
+        );
+
+        require(success, "Transaction failed");
+
+        vm.stopBroadcast();
     }
 }
